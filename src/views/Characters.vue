27e6b9719111f5bc00478efbe33929a2e2fde2a2
Jakub Michniewicz <jmic@tapptic.com>
2019-07-09 17:19:53 +0200

    Add character slider

--- src/views/Characters.vue
+++ src/views/Characters.vue
@@ -6,98 +6,114 @@
         <br />
       </div>
     </div>
-    <div class="row">
-      <b-carousel
-        id="carousel-1"
-        v-model="slide"
-        :interval="4000"
-        controls
-        indicators
-        style="text-shadow: 1px 1px 2px #333;"
-        @sliding-start="onSlideStart"
-        @sliding-end="onSlideEnd"
-      >
-        <!-- <b-carousel-slide>
-          <img
-            slot="img"
-            class="d-block img-fluid w-100"
-            width="1024"
-            height="480"
-            background="@/assets/glow_assa.png"
-            alt="image slot"
-          />
-        </b-carousel-slide>
-
-        <b-carousel-slide>
-          <img
-            slot="img"
-            class="d-block img-fluid w-100"
-            width="1024"
-            height="480"
-            background="@/assets/glow_berserk.png"
-            alt="image slot"
-          />
-        </b-carousel-slide>
-
-        <b-carousel-slide>
-          <img
-            slot="img"
-            class="d-block img-fluid w-100"
-            width="100%"
-            height="300px"
-            background="@/assets/glow_mage.png"
-            alt="image slot"
-          />
-        </b-carousel-slide>
-
-        <b-carousel-slide>
-          <img
-            slot="img"
-            class="d-block img-fluid w-100"
-            width="1024"
-            height="480"
-            background="@/assets/glow_hunter.png"
-            alt="image slot"
-          />
-        </b-carousel-slide> -->
-
-        <b-carousel-slide background="/assets/glow_warlock.png">
-          <img
-            slot="img"
-            class="d-block img-fluid w-100"
-            width="1024"
-            height="480"
-            src="@/assets/warlock.png"
-            alt="image slot"
-          />
-        </b-carousel-slide>
-
-        <b-carousel-slide background="/assets/glow_warrior.png">
-          <img
-            slot="img"
-            class="d-block img-fluid w-100"
-            width="1024"
-            height="480"
-            src="@/assets/warrior.png"
-            alt="image slot"
-          />
-        </b-carousel-slide>
-      </b-carousel>
+    <div class="row ">
+      <div class="col mx-auto">
+        <slick ref="slick" :options="slickOptions">
+          <div class="slide">
+            <img src="@/assets/glow_warrior.png" class="char-bg" />
+            <img src="@/assets/warrior.png" class="char" alt="" />
+          </div>
+          <div class="slide">
+            <img src="@/assets/glow_warlock.png" class="char-bg" style="top:45px;" />
+            <img src="@/assets/warlock.png" class="char" alt="" />
+          </div>
+          <div class="slide">
+            <img src="@/assets/glow_hunter.png" class="char-bg" style="left:calc(50% - 175px);" />
+            <img src="@/assets/hunter.png" class="char" alt="" />
+          </div>
+          <div class="slide">
+            <img src="@/assets/glow_berserk.png" class="char-bg" style="top:52px;left:calc(50% - 138px);" />
+            <img src="@/assets/berserk.png" class="char" alt="" />
+          </div>
+          <div class="slide">
+            <img src="@/assets/glow_mage.png" class="char-bg" style="top:40px;"/>
+            <img src="@/assets/mage.png" class="char" alt="" />
+          </div>
+          <div class="slide">
+            <img src="@/assets/glow_warrior.png" class="char-bg" style="left:calc(50% - 140px);" />
+            <img src="@/assets/assassine.png" class="char" alt="" />
+          </div>
+        </slick>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
+import Slick from 'vue-slick'
+
 export default {
-  name: 'Characters'
+  name: 'Characters',
+  components: {
+    Slick
+  },
+  data() {
+    return {
+      slickOptions: {
+        slidesToShow: 1,
+        autoplay: true,
+        autoplaySpeed: 5000,
+        adaptiveHeight: true,
+        prevArrow:
+          '<div class="slick-prev slick-arrow" aria-label="Previous"></div>',
+        nextArrow:
+          '<div class="slick-next slick-arrow" aria-label="Previous"></div>'
+        // Any other options that can be got from plugin documentation
+      }
+    }
+  }
 }
 </script>
 
 <style lang="scss">
-.carousel-control-prev-icon {
-  background-image: url('/car_left.png');
-}
-.carousel-control-next-icon {
-  background-image: url('/car_right.png');
+.slick {
+  &-slider &-track,
+  &-slider &-list {
+    overflow: visible;
+  }
+  &-arrow {
+    height: 35px;
+    width: 25px;
+    position: absolute;
+    z-index: 10;
+    top: 50%;
+    padding: 0;
+    -webkit-transform: translate(0, -50%);
+    -ms-transform: translate(0, -50%);
+    transform: translate(0, -50%);
+    cursor: pointer;
+    color: transparent;
+    border: none;
+    outline: none;
+    background: transparent;
+  }
+  &-prev {
+    background: url('/car_left.png') center / contain;
+    left: -25px;
+  }
+  &-next {
+    background: url('/car_right.png') center / contain;
+    right: -25px;
+  }
+  &-slider {
+    .slide {
+      position: relative;
+      img {
+        height: 350px;
+        margin: auto;
+        margin-bottom: 45px;
+      }
+      .char {
+        position: relative;
+        z-index: 5;
+      }
+      .char-bg {
+        position: absolute;
+        top: 60px;
+        left: calc(50% - 125px);
+        z-index: 4;
+      }
+    }
+  }
 }
 </style>
